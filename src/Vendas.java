e994662f0e061776a864ae9c8f14bea8b1af906e
Agbl09 <gabriela.souza.investimentos@gmail.com>
2024-08-26 18:29:36 -0300

    Adicionada a tela Venda com a tabela

---
+++ src/Vendas.java
@@ -0,0 +1,103 @@
+
+import java.util.ArrayList;
+import javax.swing.table.DefaultTableModel;
+
+
+public class Vendas extends javax.swing.JPanel {
+
+    
+    public Vendas() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        jLabel1.setFont(new java.awt.Font("Lucida Fax", 0, 18)); // NOI18N
+        jLabel1.setText("Lista de Produtos Vendidos");
+
+        jTable1.setModel(listarProdutosVendidos());
+        jScrollPane2.setViewportView(jTable1);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(206, 206, 206)
+                .addComponent(jLabel1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(80, Short.MAX_VALUE)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 518, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(78, 78, 78))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(jLabel1)
+                .addGap(48, 48, 48)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 286, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(105, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    // End of variables declaration//GEN-END:variables
+
+public DefaultTableModel listarProdutosVendidos(){
+        DefaultTableModel model = null;
+        
+        try {
+            ProdutosDAO produtosdao = new ProdutosDAO();
+            
+            model = (DefaultTableModel) jTable1.getModel();
+            model.setNumRows(0);
+            
+            String[] coluna = {"ID","NOME","VALOR","STATUS"};
+            
+            model.setColumnIdentifiers(coluna);
+            
+            ArrayList<ProdutosDTO> listagem = produtosdao.listarProdutosVendidos();
+            
+            for(int i = 0; i < listagem.size(); i++){
+                model.addRow(new Object[]{
+                    listagem.get(i).getId(),
+                    listagem.get(i).getNome(),
+                    listagem.get(i).getValor(),
+                    listagem.get(i).getStatus()
+                });
+             
+            }
+     
+
+        } catch (Exception e) {
+        }
+         return model;  
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
